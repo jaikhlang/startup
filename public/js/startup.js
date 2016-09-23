bf624690891ae75dcb939b7b451dc1dc5b6a279d
w00p <kjetil@stiligt.no>
2016-09-23 13:36:03 +0200

    Updated js file

--- public/js/startup.js
+++ public/js/startup.js
@@ -1,13 +1,12 @@
  $(document).ready(function () {
-     
-            window.setTimeout(function() {
-                $(".autoclose").fadeTo(1500, 0).slideUp(500, function(){
-                    $(this).remove(); 
+    
+    window.setTimeout(function() {
+        $(".autoclose").fadeTo(1500, 0).slideUp(500, function(){
+            $(this).remove(); 
                 });
             }, 2000);
-         
-        });
 
-        $(".delete").on("submit", function(){
-            return confirm("Do you want to delete this item?");
-        }); 
+$('[data-toggle="tooltip"]').tooltip();             
+});
+
+var simplemde = new SimpleMDE();
